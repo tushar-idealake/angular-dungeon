36666f6d57ccd663c72ee3c86751980060ee0393
Joshua Morony <joshua.morony@gmail.com>
2025-06-12 21:29:15 +0930

    adjust spawn position

--- src/app/dungeon/entities/player.component.ts
+++ src/app/dungeon/entities/player.component.ts
@@ -9,7 +9,7 @@ import { Object3D, Vector3 } from 'three';
     <ngt-object3D
       #player
       ngtrRigidBody
-      [position]="[-(layout().length / 2 + 3), 0.5, 0]"
+      [position]="[-(layout().length / 2), 0.5, 0.5]"
       [options]="{
         mass: 1,
         enabledRotations: [false, false, false],
