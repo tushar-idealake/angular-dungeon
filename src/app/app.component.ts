74646b9fe58fd2db9d056317095ee0ccd91247bc
Tushar Kadam <tushar_kadam@idealake.com>
2025-06-23 16:41:59 +0530

    changes to observe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,17 @@ import { Dungeon } from './dungeon/dungeon.component';
 @Component({
   selector: 'app-root',
   template: `
+        <span style="
+        position: fixed;
+        top: 20px;
+        left: 20px;
+        z-index: 1000;
+        background: transparent;
+        color: black;
+        font-size: 1.5rem;
+      ">
+        Player health : 99.9999%
+      </span>
     <ngt-canvas>
       <dungeon-scene *canvasContent />
     </ngt-canvas>
@@ -12,4 +23,4 @@ import { Dungeon } from './dungeon/dungeon.component';
   host: { class: 'block h-dvh w-full' },
   imports: [NgtCanvas, Dungeon, NgtCanvas],
 })
-export class AppComponent {}
+export class AppComponent { }
